a7b2eb42df2af8838c93bc20ff0e821b261c7462
Jared Lunde <jared.lunde@gmail.com>
2021-08-26 20:08:16 -0600

    chore: add global styles to storybook stories

--- .storybook/preview.js
+++ .storybook/preview.js
@@ -1,9 +1,11 @@
 import * as React from "react";
 import { RouterContext } from "next/dist/shared/lib/router-context";
-import { DashProvider } from "@dash-ui/react";
+import { DashProvider, useGlobal } from "@dash-ui/react";
+import resetGlobalStyles from "@dash-ui/reset";
 import { addDecorator } from "@storybook/react";
 import { configureActions } from "@storybook/addon-actions";
 import { styles } from "../styles";
+import { typography } from "../styles/text";
 
 export const parameters = {
   actions: { argTypesRegex: "^on[A-Z].*" },
@@ -17,9 +19,13 @@ export const parameters = {
    * @see https://storybook.js.org/addons/storybook-addon-themes
    */
   themes: {
-    default: "Light",
     list: [
-      { name: "Light", class: styles.theme("light"), color: "#fff" },
+      {
+        name: "Light",
+        class: styles.theme("light"),
+        color: "#fff",
+        default: true,
+      },
       { name: "Dark", class: styles.theme("dark"), color: "#000" },
     ],
   },
@@ -32,10 +38,65 @@ export const parameters = {
 };
 
 addDecorator((storyFn) => (
-  <DashProvider styles={styles}>{storyFn()}</DashProvider>
+  <DashProvider styles={styles}>
+    <GlobalStyles />
+    {storyFn()}
+  </DashProvider>
 ));
 
 configureActions({
   depth: 3,
   limit: 20,
 });
+
+function GlobalStyles() {
+  useGlobal(resetGlobalStyles, []);
+
+  useGlobal(
+    ({ color, font, transition }) => ({
+      "*, ::before, ::after, body": {
+        position: "relative",
+        margin: 0,
+        overflowWrap: "break-word",
+      },
+      "*:focus": {
+        outline: "none",
+      },
+      "::selection, ::-moz-selection": {
+        backgroundColor: color.indigo200,
+      },
+      html: {
+        fontSize: "100%",
+        overflowX: "hidden",
+
+        ":focus-within": {
+          scrollBehavior: "smooth",
+        },
+      },
+      body: {
+        minWidth: "100%",
+        minHeight: "100%",
+        backgroundColor: color.bodyBg,
+        fontFamily: font.family.sans,
+      },
+      ".loud": {
+        transitionProperty: "opacity,visibility",
+        transitionDuration: transition.duration.slower,
+        transitionTimingFunction: transition.timing.inOut,
+      },
+      ".writing-mode-enabled .loud": {
+        opacity: "0!important",
+        visibility: "hidden",
+      },
+      ".writing-mode-disabled .loud": {
+        opacity: 1,
+        visibility: "visible",
+      },
+    }),
+    []
+  );
+
+  useGlobal(`body {${typography.css("sm")}}`, []);
+
+  return null;
+}
