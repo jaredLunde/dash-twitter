b893ddf986a5d388edef50628cab3d80febff105
Jared Lunde <jared.lunde@gmail.com>
2021-09-10 22:07:13 -0600

    silence validators

--- next.config.js
+++ next.config.js
@@ -5,9 +5,13 @@ module.exports = {
     domains: ["pbs.twimg.com"],
   },
   eslint: {
+    // Turns off ESLint during builds becaues we do it in an
+    // earlier CI step
     ignoreDuringBuilds: true,
   },
   typescript: {
+    // Turns off type checking during builds becaues we do it in an
+    // earlier CI step
     ignoreBuildErrors: true,
   },
 };
