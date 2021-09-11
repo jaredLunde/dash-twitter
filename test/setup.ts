c923528df874d437d698dba2d585ba052e6327c8
Jared Lunde <jared.lunde@gmail.com>
2021-09-10 22:43:43 -0600

    add next router mock

--- test/setup.ts
+++ test/setup.ts
@@ -1,8 +1,12 @@
 import "@testing-library/jest-dom/extend-expect";
-import serializer, { matchers } from "@dash-ui/jest";
+import { matchers } from "@dash-ui/jest";
 
 expect.extend(matchers);
-expect.addSnapshotSerializer(serializer);
+
+/**
+ * @see https://github.com/scottrippey/next-router-mock
+ */
+jest.mock("next/dist/client/router", () => require("next-router-mock"));
 
 afterEach(() => {
   jest.clearAllMocks();
